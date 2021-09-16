322f7c957f8a1e3c7480929fb5d486fe25b0f282
joaogomes <joaoguilherme102030@gmail.com>
2021-09-15 21:13:28 -0300

    Implement support to navigation for NavHostFragment in MainActivity

--- app/src/main/java/com/portifolio/todoapp/MainActivity.kt
+++ app/src/main/java/com/portifolio/todoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.portifolio.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.portifolio.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setContentView(binding.root)
+
+
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
