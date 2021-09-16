7f07ae3b78b09e5a3b5cec6fcac47bcdb8277b47
joaogomes <joaoguilherme102030@gmail.com>
2021-09-15 21:14:02 -0300

    Implement onDestroy and FAB click inside ListFragment

--- app/src/main/java/com/portifolio/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/list/ListFragment.kt
@@ -5,19 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.portifolio.todoapp.R
+import com.portifolio.todoapp.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
 
+    private var _binding : FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+
+        _binding = FragmentListBinding.inflate(layoutInflater)
+
+        binding.floatingActionButton.setOnClickListener{
+
+            val action = ListFragmentDirections.actionListFragmentToAddFragment()
+
+            findNavController().navigate(action)
+
+        }
+
+        return binding.root
     }
 
 
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
