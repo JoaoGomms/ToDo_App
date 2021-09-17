18151261eda8088134c9dbeae2886cf8e26db901
joaogomes <joaoguilherme102030@gmail.com>
2021-09-16 23:39:59 -0300

    Add some menu itens on Fragments
    UpdateFragment have a save button and delete button now
    AddFragment havbe a finish button

--- app/src/main/java/com/portifolio/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/add/AddFragment.kt
@@ -1,20 +1,31 @@
 package com.portifolio.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.portifolio.todoapp.R
+import com.portifolio.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+
+    private var _binding : FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+        _binding = FragmentAddBinding.inflate(layoutInflater)
+
+        setHasOptionsMenu(true)
+
+
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
 }
--- app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
@@ -1,20 +1,29 @@
 package com.portifolio.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.portifolio.todoapp.R
+import com.portifolio.todoapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+
+        _binding = FragmentUpdateBinding.inflate(layoutInflater)
+
+        setHasOptionsMenu(true)
+
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 }
