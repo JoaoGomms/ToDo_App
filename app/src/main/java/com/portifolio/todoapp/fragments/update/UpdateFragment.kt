ae0cff828f48a3bd73c3a8864d1922b2e1ed7f4d
joaogomes <joaoguilherme102030@gmail.com>
2021-09-19 19:05:06 -0300

    Update fragment now receive argument as TodoEntity

--- app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
@@ -3,10 +3,13 @@ package com.portifolio.todoapp.fragments.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.portifolio.todoapp.R
 import com.portifolio.todoapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+
+    private val args by navArgs<UpdateFragmentArgs>()
 
     private var _binding: FragmentUpdateBinding? = null
     private val binding get() = _binding!!
@@ -18,9 +21,16 @@ class UpdateFragment : Fragment() {
 
         _binding = FragmentUpdateBinding.inflate(layoutInflater)
 
+        bindViews()
+
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    private fun bindViews() {
+        binding.textTitleEditText.setText(args.currentTodo.title)
+        binding.descriptionEditText.setText(args.currentTodo.description)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
