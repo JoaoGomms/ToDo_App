9cc9daf7c40c0d4447b3b7099d1ca542e826e4cb
joaogomes <joaoguilherme102030@gmail.com>
2021-09-20 21:10:18 -0300

    Instantiate viewModels and action for Update Fragment Set data on Update Fragment
    Set listener to Spinner

--- app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/update/UpdateFragment.kt
@@ -2,14 +2,23 @@ package com.portifolio.todoapp.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.portifolio.todoapp.R
+import com.portifolio.todoapp.data.model.TodoEntity
 import com.portifolio.todoapp.databinding.FragmentUpdateBinding
+import com.portifolio.todoapp.viewmodel.SharedViewModel
+import com.portifolio.todoapp.viewmodel.TodoViewModel
 
 class UpdateFragment : Fragment() {
 
     private val args by navArgs<UpdateFragmentArgs>()
+    private val sharedViewModel: SharedViewModel by viewModels()
+    private val todoViewModel: TodoViewModel by viewModels()
+    private val action = UpdateFragmentDirections.actionUpdateFragmentToListFragment()
 
     private var _binding: FragmentUpdateBinding? = null
     private val binding get() = _binding!!
@@ -31,6 +40,8 @@ class UpdateFragment : Fragment() {
     private fun bindViews() {
         binding.textTitleEditText.setText(args.currentTodo.title)
         binding.descriptionEditText.setText(args.currentTodo.description)
+        binding.prioritiesSpinner.setSelection(sharedViewModel.parsePriorityToInt(args.currentTodo.priority))
+        binding.prioritiesSpinner.onItemSelectedListener = sharedViewModel.listener
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
