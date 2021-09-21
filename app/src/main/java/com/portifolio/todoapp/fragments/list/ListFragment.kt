aabb9c5aa75239aed35f6aba1a5aa55f19efdba4
joaogomes <joaoguilherme102030@gmail.com>
2021-09-21 00:43:57 -0300

    Implement deleteAll function to list fragment Implement image and text that only appears when database is empty
    Implement add new todo when click on image view that only appears when database is empty

--- app/src/main/java/com/portifolio/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/portifolio/todoapp/fragments/list/ListFragment.kt
@@ -1,7 +1,9 @@
 package com.portifolio.todoapp.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,10 +16,11 @@ import com.portifolio.todoapp.viewmodel.TodoViewModel
 
 class ListFragment : Fragment() {
 
-    private var _binding : FragmentListBinding? = null
+    private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
     private val adapter: TodoAdapter by lazy { TodoAdapter() }
     private val todoViewModel: TodoViewModel by viewModels()
+    val toAddFragmentAction = ListFragmentDirections.actionListFragmentToAddFragment()
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
 
@@ -34,12 +37,18 @@ class ListFragment : Fragment() {
 
         binding.listRecyclerView.adapter = adapter
         binding.listRecyclerView.layoutManager = GridLayoutManager(context, 1)
-
+        handleClicks()
         todoViewModel.getTodos().observe(viewLifecycleOwner, {
-                data -> adapter.setData(data)
+
+                data ->
+            adapter.setData(data)
+            if (data.isEmpty()) {
+                binding.addFirstTextView.visibility = View.VISIBLE
+                binding.addFirstImageView.visibility = View.VISIBLE
+            }
         })
 
-        handleClicks()
+
 
         setHasOptionsMenu(true)
 
@@ -47,11 +56,14 @@ class ListFragment : Fragment() {
     }
 
 
-    private fun handleClicks(){
+    private fun handleClicks() {
 
-        binding.floatingActionButton.setOnClickListener{
-            val action = ListFragmentDirections.actionListFragmentToAddFragment()
-            findNavController().navigate(action)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(toAddFragmentAction)
+        }
+
+        binding.addFirstImageView.setOnClickListener {
+            findNavController().navigate(toAddFragmentAction)
         }
 
     }
@@ -59,5 +71,31 @@ class ListFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_delete_all -> confirmeDeleteAll()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmeDeleteAll() {
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setTitle("Delete all ToDos?")
+        builder.setMessage("This action can't be undone")
+
+        builder.setPositiveButton("Yes") { _, _ ->
+            todoViewModel.deleteAll()
+            Toast.makeText(requireContext(), "All files deleted", Toast.LENGTH_SHORT).show()
+
+        }
+
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.create().show()
+
     }
 }
